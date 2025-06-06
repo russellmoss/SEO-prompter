3433ca511c6c06edce477efe1bde04942df2dabd
Russsell <russell@mileaestatevineyard.com>
2025-06-06 19:36:53 -0400

    Update Next.js configuration for static export

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
+  output: 'export',
   reactStrictMode: true,
-  swcMinify: true,
+  images: {
+    unoptimized: true
+  },
+  trailingSlash: true
 }
 
 module.exports = nextConfig 
